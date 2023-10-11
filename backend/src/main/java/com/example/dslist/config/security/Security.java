5ca9884344ee6f34f8e40169fe495a101d305e7b
João <joao.sanches7@fatec.sp.gov.br>
2023-10-10 21:22:57 -0300

    Função: Filtro de segurança modificado para Stateless, DaoAuthProvider deletado

--- backend/src/main/java/com/example/dslist/config/security/Security.java
+++ backend/src/main/java/com/example/dslist/config/security/Security.java
@@ -1,62 +1,54 @@
 package com.example.dslist.config.security;
 
-import com.example.dslist.services.DsListUserDetailsService;
+import com.example.dslist.services.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class Security {
     @Autowired
-    private DsListUserDetailsService service;
+    private AuthorizationService service;
+
+    @Autowired
+    private JwtSecurityFilter filter;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(service);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
-        http.csrf(csrf -> csrf.disable())
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/home",
+                    .requestMatchers("/home",
                         "/login",
                         "/error",
                         "/register/**",
                         "/forgot-password/**",
                         "/rest-register/**",
                         "/rest-forgot-password/**",
-                        "/rest-users"
-                )
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-        ).formLogin(formLogin -> formLogin
-                .loginPage("/login")
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                .defaultSuccessUrl("/home", true)
-                .permitAll())
-                .logout(logout -> logout
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login"));
-        return http.build();
+                        "/rest-users", "/rest-login"
+                    ).permitAll().anyRequest().authenticated()
+                ).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).build();
     }
 }
