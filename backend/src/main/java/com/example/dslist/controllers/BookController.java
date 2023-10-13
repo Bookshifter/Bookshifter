4ce6493c51c3c92f99e4e65dc0d2261a759e15d7
João <joao.sanches7@fatec.sp.gov.br>
2023-10-13 19:52:36 -0300

    Função: Controle de Livro completo

---
+++ backend/src/main/java/com/example/dslist/controllers/BookController.java
@@ -0,0 +1,48 @@
+package com.example.dslist.controllers;
+
+import com.example.dslist.bookapi.openlibrary.FullRequestOpenLibrary;
+import com.example.dslist.dto.BookDTO;
+import com.example.dslist.services.interfaces.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private RestTemplate template;
+
+    @Autowired
+    private BookService service;
+    public static Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    @GetMapping("/isbn:{isbn}")
+    public String getBook(@PathVariable Long isbn){
+        service.saveBookByIsbn(isbn);
+        return "Livro adicionado!";
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<BookDTO>> findAll(){
+        return ResponseEntity.ok(service.findAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Long id){
+        service.deleteBook(id);
+        return "Livro deletado com sucesso!";
+    }
+
+}
