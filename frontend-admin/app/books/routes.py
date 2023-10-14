29c5571d94199e3b05b7fe06e67152b8e0356881
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2023-10-14 17:40:20 -0300

    Refatoração: Removendo trechos inutilizados

--- frontend-admin/app/books/routes.py
+++ frontend-admin/app/books/routes.py
@@ -1,12 +1,10 @@
-from flask import render_template, jsonify
+from flask import render_template
 from app.books import bp, api
 from flask import current_app
 
-import json
 @bp.route('/books')
 def books():
     backend_url = current_app.config.get('BACKEND_API_URL')
     books = api.get_api_books(f'{backend_url}books/all')
-    print(books)
     return render_template('/books/books.html',
                            books=books)
