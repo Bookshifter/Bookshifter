2c3daf7669dc969d4b5810dbec93cbeef7163f40
João <joao.sanches7@fatec.sp.gov.br>
2023-10-21 20:47:33 -0300

    Refatoração/Falha:Sugestao do SonarLint revertida

--- backend/src/main/java/com/example/bookshifter/entities/User.java
+++ backend/src/main/java/com/example/bookshifter/entities/User.java
@@ -25,7 +25,7 @@ public class User implements UserDetails {
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "tb_user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
     inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    transient private Collection<Role> roles;
+    private Collection<Role> roles;
 
     public User(){
     }
