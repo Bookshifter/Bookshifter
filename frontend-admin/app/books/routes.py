e3f9e47df31dd8aad76f3af11ff1d7c5e7d1ec4d
Gustavo Oliveira <gsantos@nic.br>
2023-10-23 19:57:53 -0300

    Função: Adicionando fatecs mockado

--- frontend-admin/app/books/routes.py
+++ frontend-admin/app/books/routes.py
@@ -11,6 +11,26 @@ def books():
         return redirect(url_for('authentication.login'))
     backend_url = current_app.config.get('BACKEND_API_URL')
     books = api.get_api_books(f'{backend_url}books/all')
-    response = make_response(render_template('/books/books.html', backend_url=backend_url, books=books))
+    # Resgatar fatecs via API
+    fatecs = [
+        {
+            'id': 1,
+            'name': 'Fatec Diadema'
+        },
+        {
+            'id': 2,
+            'name': 'Fatec Zona Leste'
+        },{
+            'id': 3,
+            'name': 'Fatec Praia Grande'
+        },{
+            'id': 4,
+            'name': 'Fatec Zona Sul'
+        },{
+            'id': 5,
+            'name': 'Fatec Ferraz de Vasconcelos'
+        }
+    ]
+    response = make_response(render_template('/books/books.html', backend_url=backend_url, books=books, fatecs=fatecs))
     response.headers['Authorization'] = f"Bearer {session['token']}"
     return response
