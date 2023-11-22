e35ce93b8b711a23511d68cf34d3a19c991c1b77
João <joaokavalciuki@hotmail.com>
2023-11-22 16:52:08 -0300

    Função Projeto em conformidade com a dev

--- backend/src/main/java/com/example/bookshifter/BookshifterApplication.java
+++ backend/src/main/java/com/example/bookshifter/BookshifterApplication.java
@@ -2,16 +2,24 @@ package com.example.bookshifter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 
+@ServletComponentScan
+public class BookshifterApplication extends SpringBootServletInitializer {
 
-public class BookshifterApplication {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookshifterApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookshifterApplication.class, args);
 	}
+
 
 }
