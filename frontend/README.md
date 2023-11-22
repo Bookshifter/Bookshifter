84567cce0014797b59a5c06c6931514ef0d73eac
João <joao.sacnhes7@fatec.p.gov.br>
2023-11-22 16:03:44 -0300

    Revert "5454354543"
    This reverts commit 6fb030c21eb5810626a2c3ae2b5ab242e131b062.

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+# Bookshifter - Flask
+
+#### Para iniciar o projeto
+
+```
+\bookshifter-flask> pip install -r requirements.txt
+\bookshifter-flask> python.exe -m flask run --debug --port=8000
+```
---
+++ frontend/app/ecommerce/routes.py
@@ -0,0 +1,75 @@
+from flask import render_template, redirect, url_for, session, flash, request
+from app.ecommerce import bp
+from flask import current_app
+from app.authentication import functions as auth
+from app.books import api as books_api
+import json
+
+@bp.route('/')
+def index():
+  print(session)
+  return render_template('/ecommerce/index.html', page="index")
+
+@bp.route('/about')
+def about():
+  return render_template('/ecommerce/about.html', page="about")
+
+@bp.route('/cart')
+def cart():
+  return render_template('/ecommerce/cart.html')
+
+@bp.route('/checkout')
+def checkout():
+  return render_template('/ecommerce/checkout.html')
+
+@bp.route('/contact')
+def contact():
+  return render_template('/ecommerce/contact.html', page="contact")
+
+@bp.route('/summary')
+def summary():
+  return render_template('/ecommerce/summary.html', page="summary")
+
+@bp.route('/shop')
+def shop():
+  return render_template('/ecommerce/shop.html', page="shop")
+
+@bp.route('/search')
+def search():
+  query = request.args.get('query')
+  backend_url = current_app.config.get('BACKEND_API_URL')
+  search_url = backend_url + 'books/search?query=' + query
+  params = {
+    'method': 'GET',
+    'url': search_url,
+    'token': session['token'] if 'token' in session else None
+  }
+  response = books_api.api_books(params)
+  if type(response) != dict:
+    response = eval(response)
+    
+  # if 'error' in response:
+  #   flash(response['error'], 'danger')
+  return render_template('/ecommerce/search.html', page="shop", books=response)
+
+@bp.route('/single-news')
+def single_news():
+  return render_template('/ecommerce/single-news.html')
+
+@bp.route('/book/<book_id>')
+def book(book_id):
+  backend_url = current_app.config.get('BACKEND_API_URL')
+  book_url = backend_url + f'books/{book_id}' 
+  params = {
+    'method': 'GET',
+    'url': book_url,
+    'token': session['token'] if 'token' in session else None
+  }
+  response = books_api.api_books(params)
+  if type(response) != dict:
+    response = eval(response)    
+  return render_template('/ecommerce/product.html', book=response)
+
+@bp.route('/error-page')
+def error_page():
+  return render_template('/ecommerce/error-page.html')
